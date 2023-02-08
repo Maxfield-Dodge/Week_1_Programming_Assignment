c7d324eee42ff4f5b4b39a48bb9ae9cd0ccabf1b
WLHS-CTE <dodgem@wlhs.wlwv.k12.or.us>
2023-02-08 09:05:42 -0800

    fixed a couple javadocs mistakes

--- src/Polygon.java
+++ src/Polygon.java
@@ -15,13 +15,11 @@ public class Polygon {
 
    private double area;
 
-   private double pi = 3.14159265359;
-
     /**
      * Creates a default polygon, a triangle with 3 sides and side length 1.0
      * and calculates the perimeter.
      */
-   public Polygon(){
+   public Polygon() {
        numSides = 3;
        sideLength = 1.0;
        shapeType = "triangle";
@@ -31,14 +29,14 @@ public class Polygon {
     /**
      * Creates a polygon with using user parameters
      * and calculates perimeter.
-     * @param ns desired number of sides
-     * @param sl desired side length
-     * @param st desired shape type
+     * @param theNumSides desired number of sides
+     * @param theSideLength desired side length
+     * @param theShapeType desired shape type
      */
-   public Polygon(int ns, double sl, String st){
-           numSides = ns;
-           sideLength = sl;
-           shapeType = st;
+   public Polygon(int theNumSides, double theSideLength, String theShapeType) {
+           numSides = theNumSides;
+           sideLength = theSideLength;
+           shapeType = theShapeType;
            perimeter = sideLength * numSides;
    }
 
@@ -50,39 +48,39 @@ public class Polygon {
         return sideLength;
     }
 
-    public String getShapeType(){
+    public String getShapeType() {
         return shapeType;
     }
 
     /**
      * Changes number of sides of polygon
-     * @param ns desired numbers of sides
+     * @param newNumSides desired numbers of sides
      */
-    public void setNumSides(int ns){
-        numSides = ns;
+    public void setNumSides(int newNumSides) {
+        numSides = newNumSides;
     }
 
     /**
      * Changes side length of polygon
-     * @param sl desired side length
+     * @param newSideLength desired side length
      */
-    public void setSideLength(double sl){
-        sideLength = sl;
+    public void setSideLength(double newSideLength) {
+        sideLength = newSideLength;
     }
 
     /**
      *Changes shape type of polygon
-     * @param st desired shape type
+     * @param newShapeType desired shape type
      */
-    public void setShapeType(String st){
-        shapeType = st;
+    public void setShapeType(String newShapeType) {
+        shapeType = newShapeType;
     }
 
     /**
      * returns the perimeter
      * @return
      */
-    public String calculatePerimeter(){
+    public String calculatePerimeter() {
        return "It has a perimeter of " + perimeter;
     }
 
@@ -90,9 +88,9 @@ public class Polygon {
      * Calculates the area and returns formatted to 3 decimal places
      * @return area with 3 decimal places
      */
-    public String calculateArea(){
+    public String calculateArea() {
         DecimalFormat df = new DecimalFormat("###.###");
-        apothem = Math.toRadians(180/((double)numSides));
+        apothem = Math.toRadians(180/((double) numSides));
         apothem = Math.tan(apothem);
         apothem *= 2;
         apothem = sideLength / apothem;
@@ -101,10 +99,12 @@ public class Polygon {
     }
 
     /**
-     * Checks if a polygon is valid and returns either invalid or the attributes of the object
-     * @return invalid or the shape type, numbers of sides, side length, and perimeter formatted to 3 decimals places
+     * Checks if a polygon is valid and returns either
+     * invalid or the attributes of the object
+     * @return invalid or the shape type, numbers of sides,
+     * side length, and perimeter formatted to 3 decimals places
      */
-    public String toString(){
+    public String toString() {
        DecimalFormat df = new DecimalFormat("###.###");
         if (numSides < 3 || sideLength < 0){
             return "Not a valid polygon";
